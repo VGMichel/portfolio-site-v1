e0d9b60f3453a70d810e65c374fcfcee0ea29835
Valerie <32314887+VGMichel@users.noreply.github.com>
2022-03-22 20:01:28 -0400

    Update 3/22
    Tweaking navigation/menu
    Implement JS navigation
    Implement media queries
    Edit CSS (project section) to be responsive for mobile

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,33 @@
-const menu = document.querySelector('.menubar');
+const menu = document.querySelector('.menu');
 const nav = document.querySelector('.nav__content');
 
+const navContent = document.querySelector('.nav');
+const closeBtn = document.querySelector('.nav__close');
+const navBtn = document.querySelector('.menubar');
+const navBar = document.querySelector('.nav__bar')
+
+
+// Sticky Nav
 window.onscroll = function(){
     if(window.pageYOffset >= nav.offsetTop) {
-        menu.classList.add("sticky");
+        menu.classList.add('sticky')
+        navBar.classList.add('sticky')
     } else {
-        menu.classList.remove("sticky");
+        menu.classList.remove('sticky')
+        navBar.classList.remove('sticky')
     }
+}
+
+// Menu Toggle
+navBtn.addEventListener('click',(e)=>{
+  navContent.classList.toggle('active');
+})
+
+closeBtn.addEventListener('click',(e)=>{
+  navContent.classList.toggle('active');
+})
+
+// Menu Toggle Function
+function navSelect() {
+    navContent.classList.toggle('active')
 }
